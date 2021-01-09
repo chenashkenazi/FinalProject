f0f1e5bd7e299483322df4568c72d2a994125e6a
chenashkenazi <58274766+chenashkenazi@users.noreply.github.com>
2021-01-09 12:16:02 +0200

    Added a simple menu, levels layout, levels activity

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.finalproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Simon");
+        Button playBtn = findViewById(R.id.play_btn);
+        Button resetBtn = findViewById(R.id.reset_game_btn);
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LevelsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Confirm restart").setMessage("Are you sure you want to reset your progress?")
+                        .setPositiveButton("Yes", new MyAlertDialogListener()).setNegativeButton("No", new MyAlertDialogListener()).setCancelable(false).show();
+            }
+        });
+
+
+    }
+
+    private class MyAlertDialogListener implements DialogInterface.OnClickListener {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            if (which == DialogInterface.BUTTON_POSITIVE) {
+                Toast.makeText(MainActivity.this, "Game progress restarted", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
