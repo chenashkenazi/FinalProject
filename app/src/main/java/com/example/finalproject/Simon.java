e166922fe3b299dd381190739d4509b6690a78f0
Shir Tishler <shir.tishler03@gmail.com>
2021-02-06 13:32:15 +0200

    הוספתי כמה דברים

--- app/src/main/java/com/example/finalproject/Simon.java
+++ app/src/main/java/com/example/finalproject/Simon.java
@@ -3,57 +3,102 @@ package com.example.finalproject;
 import java.util.Random;
 
 public class Simon {
-    private int MAX_LENGTH;
-    private int Amount_of_Image_view;
-    private   int[] array_of_moves;
-    private Random r;
+    private int maxLength; //array's size - how much moves in this subLevel
+    private int amountOfImageView; // 4/6/8 parts in simon's circle
+    private int[] arrayOfMoves; //the array of moves of the subLevel (created randomly according to amountOfImageView)
+    private final Random r;
 
-    public Simon(int MAX_LENGTH, int Amount_of_Image_view)
+    public Simon(int maxLength, int amountOfImageView)
     {
-        this.Amount_of_Image_view= Amount_of_Image_view;
-        this.MAX_LENGTH= MAX_LENGTH;
-        this.array_of_moves= new int[MAX_LENGTH];
+        this.amountOfImageView = amountOfImageView;
+        this.maxLength = maxLength;
+        this.arrayOfMoves = new int[maxLength];
         r= new Random();
     }
-    public int getMAX_LENGTH()
-    {
-        return MAX_LENGTH;
-    }
-    public void setMAX_LENGTH(int MAX_LENGTH)
-    {
-        this.MAX_LENGTH= MAX_LENGTH;
-    }
-    public int getAmount_of_Image_view()
-    {
-        return this.Amount_of_Image_view;
-    }
-    public void setAmount_of_Image_view( int Amount_of_Image_view)
-    {
-        this.Amount_of_Image_view= Amount_of_Image_view;
 
+    /*public int getMaxLength()
+    {
+        return maxLength;
     }
+    public void setMaxLength(int maxLength)
+    {
+        this.maxLength = maxLength;
+    }
+    public int getAmountOfImageView()
+    {
+        return this.amountOfImageView;
+    }
+    public void setAmountOfImageView(int amountOfImageView)
+    {
+        this.amountOfImageView = amountOfImageView;
+    }
+
     public int[] getArray_of_moves(Simon simon)
     {
-        this.array_of_moves= simon.appendValueToArray(simon);
-        return this.array_of_moves;
+        this.arrayOfMoves = simon.appendValueToArray(simon);
+        return this.arrayOfMoves;
     }
 
-    public void setArray_of_moves(int[] array_of_moves) {
-        this.array_of_moves = array_of_moves;
+    public void setArrayOfMoves(int[] arrayOfMoves) {
+        this.arrayOfMoves = arrayOfMoves;
+    }*/
+
+
+    /*Getters & Setters*/
+    public int getMaxLength() {
+        return maxLength;
     }
 
-    public int generateRandomNumber(Simon simon) {
-        return r.nextInt(simon.Amount_of_Image_view) + 1; // generate random number between 1 and 4
+    public void setMaxLength(int maxLength) {
+        this.maxLength = maxLength;
     }
 
-    public int[] appendValueToArray(Simon simon) {  // add random number to the first free position in the array
-        for (int i = 0; i < simon.MAX_LENGTH; i++) {
-            if (simon.array_of_moves[i] == 0) {
-                simon.array_of_moves[i] = generateRandomNumber(simon);
+    public int getAmountOfImageView() {
+        return amountOfImageView;
+    }
+
+    public void setAmountOfImageView(int amountOfImageView) {
+        this.amountOfImageView = amountOfImageView;
+    }
+
+    public int[] getArrayOfMoves() {
+        return arrayOfMoves;
+    }
+
+    /*public void setArrayOfMoves(int[] arrayOfMoves) {
+        this.arrayOfMoves = arrayOfMoves;
+    }*/
+    public void setArrayOfMoves() {
+        this.arrayOfMoves = appendValueToArray();
+    }
+
+    /*generating random number between 1 and amountOfImageView*/
+    public int generateRandomNumber() {
+        return r.nextInt(this.amountOfImageView) + 1;
+    }
+    /*public int generateRandomNumber(Simon simon) {
+        return r.nextInt(simon.amountOfImageView) + 1;
+    }*/
+
+    /*adding random number to the first free position in the array*/
+    public int[] appendValueToArray() {  //
+        for (int i = 0; i < this.maxLength; i++) {
+            if (this.arrayOfMoves[i] == 0) {
+                this.arrayOfMoves[i] = generateRandomNumber();
                 break;
             }
         }
-        return simon.array_of_moves;
 
+        return this.arrayOfMoves;
     }
+    /*public int[] appendValueToArray(Simon simon) {  //
+        for (int i = 0; i < simon.maxLength; i++) {
+            if (simon.arrayOfMoves[i] == 0) {
+                simon.arrayOfMoves[i] = generateRandomNumber(simon);
+                break;
+            }
+        }
+
+        return simon.arrayOfMoves;
+    }*/
 }
